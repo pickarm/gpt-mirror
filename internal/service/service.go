141395c4c7ec0f048e7caf3ef173198bbea74bac
pickarm <76402568+pickarm@users.noreply.github.com>
2026-09-01 11:34:32 +0800

    refactor: inject ChatGPT provider into services

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	chatgptprovider "PandoraHelper/internal/provider/chatgpt"
 	"PandoraHelper/internal/repository"
 	"PandoraHelper/pkg/jwt"
 	"PandoraHelper/pkg/log"
@@ -8,17 +9,25 @@ import (
 )
 
 type Service struct {
-	logger *log.Logger
-	sid    *sid.Sid
-	jwt    *jwt.JWT
-	tm     repository.Transaction
+	logger          *log.Logger
+	sid             *sid.Sid
+	jwt             *jwt.JWT
+	tm              repository.Transaction
+	chatgptProvider chatgptprovider.Provider
 }
 
-func NewService(tm repository.Transaction, logger *log.Logger, sid *sid.Sid, jwt *jwt.JWT) *Service {
+func NewService(
+	tm repository.Transaction,
+	logger *log.Logger,
+	sid *sid.Sid,
+	jwt *jwt.JWT,
+	chatgptProvider chatgptprovider.Provider,
+) *Service {
 	return &Service{
-		logger: logger,
-		sid:    sid,
-		jwt:    jwt,
-		tm:     tm,
+		logger:          logger,
+		sid:             sid,
+		jwt:             jwt,
+		tm:              tm,
+		chatgptProvider: chatgptProvider,
 	}
 }
