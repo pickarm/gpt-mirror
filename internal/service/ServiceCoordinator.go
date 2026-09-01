84d58f0b19aadbcd6e2dbe4c6227d68f365de325
pickarm <76402568+pickarm@users.noreply.github.com>
2026-09-01 12:09:03 +0800

    fix: inject credential provider into service coordinator

--- internal/service/ServiceCoordinator.go
+++ internal/service/ServiceCoordinator.go
@@ -1,6 +1,7 @@
 package service
 
 import (
+	credentialprovider "PandoraHelper/internal/provider/credential"
 	"PandoraHelper/internal/repository"
 	"github.com/spf13/viper"
 )
@@ -10,10 +11,16 @@ type Coordinator struct {
 	ShareSvc   ShareService
 }
 
-func NewServiceCoordinator(service *Service, accountRepo repository.AccountRepository, shareRepo repository.ShareRepository, viper *viper.Viper) *Coordinator {
+func NewServiceCoordinator(
+	service *Service,
+	accountRepo repository.AccountRepository,
+	shareRepo repository.ShareRepository,
+	credentialProvider credentialprovider.Provider,
+	viper *viper.Viper,
+) *Coordinator {
 	coordinator := &Coordinator{}
 
-	accountSvc := NewAccountService(service, accountRepo, viper, coordinator)
+	accountSvc := NewAccountService(service, accountRepo, credentialProvider, viper, coordinator)
 	shareSvc := NewShareService(service, shareRepo, viper, coordinator)
 
 	coordinator.AccountSvc = accountSvc
