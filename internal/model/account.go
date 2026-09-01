4b9e8c20e2ac27df9d6c9f684e50c77038a822c8
pickarm <76402568+pickarm@users.noreply.github.com>
2026-09-01 11:58:56 +0800

    security: prevent account secrets from JSON serialization

--- internal/model/account.go
+++ internal/model/account.go
@@ -3,18 +3,18 @@ package model
 type Account struct {
 	ID              uint       `json:"id" gorm:"primaryKey" gorm:"column:id"`
 	Email           string     `json:"email" gorm:"column:email"`
-	Password        string     `json:"password" gorm:"column:password"`
-	SessionToken    string     `json:"sessionToken" gorm:"column:session_token"`
-	AccessToken     string     `json:"accessToken" gorm:"column:access_token"`
+	Password        string     `json:"-" gorm:"column:password"`
+	SessionToken    string     `json:"-" gorm:"column:session_token"`
+	AccessToken     string     `json:"-" gorm:"column:access_token"`
 	CreateTime      *LocalTime `json:"createTime" gorm:"autoCreateTime" gorm:"column:create_time"`
 	UpdateTime      *LocalTime `json:"updateTime" gorm:"autoUpdateTime" gorm:"column:update_time"`
 	Shared          int        `json:"shared" gorm:"column:shared"`
-	RefreshToken    string     `json:"refreshToken" gorm:"column:refresh_token"`
+	RefreshToken    string     `json:"-" gorm:"column:refresh_token"`
 	AccountType     string     `json:"accountType" gorm:"column:account_type" gorm:"default:chatgpt"`
-	SessionKey      string     `json:"sessionKey" gorm:"column:session_key"`
+	SessionKey      string     `json:"-" gorm:"column:session_key"`
 	OneApiChannelId string     `json:"oneApiChannelId" gorm:"column:one_api_channel_id;default:''"`
-	ProxyURL        string     `json:"proxyUrl" gorm:"column:proxy_url;default:''"`
-	Shares          []Share    `gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE"`
+	ProxyURL        string     `json:"-" gorm:"column:proxy_url;default:''"`
+	Shares          []Share    `json:"-" gorm:"foreignKey:AccountID;constraint:OnDelete:CASCADE"`
 }
 
 func (m *Account) TableName() string {
