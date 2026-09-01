ebb4c7ae9c217b50b323836f2a69ae1a97190dc4
pickarm <76402568+pickarm@users.noreply.github.com>
2026-09-01 11:40:54 +0800

    feat: add deterministic proxy route resolution

---
+++ internal/transport/route.go
@@ -0,0 +1,52 @@
+package transport
+
+import "fmt"
+
+type RouteSource string
+
+const (
+	RouteSourceDirect  RouteSource = "direct"
+	RouteSourceGlobal  RouteSource = "global"
+	RouteSourceAccount RouteSource = "account"
+)
+
+type RouteInfo struct {
+	Source   RouteSource
+	Scheme   ProxyScheme
+	ProxyURL string
+}
+
+type route struct {
+	info  RouteInfo
+	proxy *ProxySpec
+}
+
+func resolveRoute(accountProxyURL, globalProxyURL string) (route, error) {
+	if accountProxyURL != "" {
+		spec, err := ParseProxy(accountProxyURL)
+		if err != nil {
+			return route{}, fmt.Errorf("account proxy: %w", err)
+		}
+		return route{
+			info: RouteInfo{Source: RouteSourceAccount, Scheme: spec.Scheme(), ProxyURL: spec.Redacted()},
+			proxy: &spec,
+		}, nil
+	}
+	if globalProxyURL != "" {
+		spec, err := ParseProxy(globalProxyURL)
+		if err != nil {
+			return route{}, fmt.Errorf("global proxy: %w", err)
+		}
+		return route{
+			info: RouteInfo{Source: RouteSourceGlobal, Scheme: spec.Scheme(), ProxyURL: spec.Redacted()},
+			proxy: &spec,
+		}, nil
+	}
+	return route{info: RouteInfo{Source: RouteSourceDirect}}, nil
+}
+
+// ResolveRoute exposes only redacted route metadata.
+func ResolveRoute(accountProxyURL, globalProxyURL string) (RouteInfo, error) {
+	route, err := resolveRoute(accountProxyURL, globalProxyURL)
+	return route.info, err
+}
